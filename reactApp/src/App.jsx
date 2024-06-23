ad422d44ca3a76fa9bae9413cfe4f7962d3b1faa
usgam <sugamn9019@gmail.com>
2024-06-23 14:40:30 +0530

    OnChange()

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -1,18 +1,67 @@
-import List from "./Components/List";
+import { useState } from "react";
 
 function App() {
 
-  const students=[{key:1,name:"sugam",marks:90},
-                  {key:2,name:"srinath",marks:89},
-                  {key:3,name:"akash",marks:92},
-                  {key:4,name:"kishore",marks:100},
-                  {key:5,name:"varun",marks:95}];
+  let [text,setText]=useState("Empty");
+  let [number,setNumber]=useState(0);
+  let [select,setSelect]=useState("Empty");
+  let [radio,setRadio]=useState("Offline");
 
-  return (
+  const changeText=(event)=>{
+    setText(event.target.value);
+  }
+
+  const changeNumber=(event)=>{
+    setNumber(event.target.value);
+  }
+
+  const changeSelect=(event)=>{
+    setSelect(event.target.value);
+  }
+
+  const changeRadio=(event)=>{
+    setRadio(event.target.value)
+  }
+
+  return(
     <>
-      <List items={students}/>
+      <input type="text" onChange={changeText}  placeholder="Name"/>
+      <p>Name : {text}</p>
+      <p>------------------------------</p>
+      
+      <input type="number"  value={number} onChange={changeNumber} placeholder="Number"/>
+      <p>Number : {number}</p>
+      <p>------------------------------</p>
+
+
+      <select onChange={changeSelect}>
+        <option value="Empty">Select transportation Mode</option>
+        <option value="Car">Car</option>
+        <option value="Bike">Bike</option>
+        <option value="Train">Train</option>
+        <option value="Flight">Flight</option>
+      </select>      
+      <p>Option : {select}</p>
+      <p>------------------------------</p>
+      
+
+      <label>
+        <input type="radio" value="Offline" onChange={changeRadio} checked={radio==="Offline"}  id="radio"/>
+        Offline
+      </label>
+      <br />
+      <label>
+        <input type="radio" value="Online" onChange={changeRadio} checked={radio==="Online"} id="radio"/>
+        Online
+      </label>
+
+      <p>Radio : {radio}</p>
+
+      
+
     </>
-  )
+    
+  );
 }
 
 export default App
